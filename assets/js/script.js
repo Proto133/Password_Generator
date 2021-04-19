480a89fe87ff11914efa8e5759e89808d4826220
Peter Roto <peterdroto@gmail.com>
2021-04-18 23:16:45 -0500

    Gsve up on trying to add event listener to JS. went with onclick from HTML.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,44 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page */
 //Possible characters to be used
-var numCharset = ["1234567890"]
-var upCharset = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var lowCharset = ["abcdefghijklmnopqrstuvwxyz"]
-var specCharset = ["!@#$%^&*()-_=+{}[];':,.<>/?"]
+var numCharset = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var upCharset = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowCharset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specCharset = [
+        "!",
+        "@",
+        "#",
+        "$",
+        "%",
+        "^",
+        "&",
+        ",",
+        "'",
+        "*",
+        "(",
+        ")",
+        ",",
+        "-",
+        ",",
+        "_",
+        "=",
+        "+",
+        "{",
+        "}",
+        "[",
+        "]",
+        ";",
+        "'",
+        ":",
+        ",",
+        ".",
+        "<",
+        ">",
+        "/",
+        "?"
+    ]
     //Get user prefences for password
 function specifyOptions() {
-    //User input variables
-    var userPref = {
-        howLong: howLong,
-        numChar: numChar,
-        lowChar: lowChar,
-        upChar: upChar,
-        specChar: specChar
-    }
-    return userPref;
     //Get length of password
     var howLong = parseInt(
         prompt("How long should your password be?")
@@ -52,11 +75,19 @@ function specifyOptions() {
         alert("If you're not taking this seriously, I'm out.");
         return;
     }
+    //User input variables
+    var userPref = {
+        howLong: howLong,
+        numChar: numChar,
+        lowChar: lowChar,
+        upChar: upChar,
+        specChar: specChar
+    };
     return userPref;
 }
 //Define randomness for genPass. Verify via console.
 function rando(char) {
-    var rIndex = Math.random() * char.length;
+    var rIndex = Math.floor(Math.random() * char.length);
     var rElement = char[rIndex];
     console.log(rElement);
     return rElement;
@@ -68,40 +99,61 @@ function genPass() {
     var passHas = [];
     var charKind = [];
 
-    // Adhere to user's rules. Verify via console.
-    if (userPref.numChar) {
+    /* Adhere to user's rules. 
+    Verify via console.*/
+
+    //Numbers 
+    if (userChoices.numChar) {
         passHas = passHas.concat(numCharset);
-        console.log(passHas);
+        //Verify
+        // console.log(passHas);
+        ////Add one number to charKind Array
         charKind.push(rando(numCharset));
 
     }
-    if (userPref.lowChar) {
+    //Lowercase
+    if (userChoices.lowChar) {
         passHas = passHas.concat(lowCharset);
-        console.log(passHas);
+        //Verify
+        // console.info(passHas);
+        //Add one lowercase to charKind Array
         charKind.push(rando(lowCharset));
     }
-    if (userPref.upChar) {
-        passHas = passHas.concat(upCharset),
-            console.log(passHas),
-            charKind.push(rando(upCharset))
+    //Uppercase
+    if (userChoices.upChar) {
+        passHas = passHas.concat(upCharset);
+        //Verify
+        // console.info(passHas);
+        //Add one uppercase to charKind Array
+        charKind.push(rando(upCharset));
     }
+    //Special characters
     if (userChoices.specChar) {
         passHas = passHas.concat(specCharset);
-        console.log(passHas);
+        //Verify
+        // console.info(passHas);
+        //Add one special character to charKind Array
         charKind.push(rando(specCharset));
     }
-    //Take if statements and put them together to meet length requirement from user input
-    for (var i = 0; i < userPref.howLong; i++) {
-        passHas = rando(passHas);
-        passArray.push(passHas);
+    //Take if statements and use the data to put together password into passArray , the ultimate culmination of all of the rules 
+    for (var i = 0; i < userChoices.howLong; i++) {
+        uniqueChar = rando(passHas);
+        passArray.push(uniqueChar);
     }
-    //Make sure that there's at least 1 of each kinds of selected charactersets. Verify via console.
+
+    /*Make sure that there's at least 1 of each kinds of selected charactersets. 
+    Verify via console.*/
     for (var i = 0; i < charKind.howLong; i++) {
         passArray[i] = charKind[i];
-        console.log("passHas: " + passHas),
-            conlose.log("charKind: " + charKind),
-            console.log("passArray: " + passArray)
-
     }
+
     return passArray.join("");
+}
+
+//function to reveal the completed password 
+function showPass() {
+    var password = genPass();
+    var passwordText = document.querySelector('#password');
+
+    passwordText.value = password;
 }
